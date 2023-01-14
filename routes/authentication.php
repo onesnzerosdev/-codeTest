1c0276ac16d40e67693380256c12aa819d3a4912
Asif Ul Islam <aseasifislam@gmail.com>
2023-01-14 12:17:57 +0600

    routes seperate

--- routes/authentication.php
+++ routes/authentication.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin', function () {
-    return view('welcome');
-});
-
-// Route::get('/dashboard', function () {
+// Route::get('/admin', function () {
 //     return view('welcome');
-// })->middleware(['auth'])->name('dashboard');
+// });
 
-// require __DIR__ . '/auth.php';
+Route::get('/dashboard', function () {
+    return view('welcome');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
